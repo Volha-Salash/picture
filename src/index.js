055a5b517323fc694b15ef02f6b8ec4371641478
Kursonchik <olgakurson@mail.ru>
2023-06-12 23:09:57 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import thunk from 'redux-thunk';
+import ImageList from './components/ImageList';
+import imageReducer from './reducers';
+
+const store = createStore(imageReducer, applyMiddleware(thunk));
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+  <ImageList />
+</Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
